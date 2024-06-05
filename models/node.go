a57fcd683098766d297a660542d38218943d9a99
vaibhavvvvv <vaibhavng7@gmail.com>
2024-06-05 22:29:48 +0530

    add NodeName in node struct

--- models/node.go
+++ models/node.go
@@ -4,9 +4,9 @@ type Node struct {
 	Id                  string  `json:"id" gorm:"primaryKey"`
 	Name                string  `json:"name"`
 	HttpPort            string  `json:"httpPort"`
+	NodeName            string  `json:"nodename"`
 	Domain              string  `json:"domain"`
 	Address             string  `json:"address"`
-	NodeName            string  `json:"nodename"`
 	Region              string  `json:"region"`
 	Status              string  `json:"status"`
 	DownloadSpeed       float64 `json:"downloadSpeed"`
