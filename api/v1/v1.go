d80fa171e693a285c086cb908f4745bccada9705
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-07-10 13:07:14 +0530

    register defi api

--- api/v1/v1.go
+++ api/v1/v1.go
@@ -5,6 +5,7 @@ import (
 	"github.com/NetSepio/erebrus-gateway/api/v1/client"
 	"github.com/NetSepio/erebrus-gateway/api/v1/nodeOperatorForm"
 	"github.com/NetSepio/erebrus-gateway/api/v1/nodes"
+	"github.com/NetSepio/erebrus-gateway/api/v1/registerDeFi"
 	"github.com/NetSepio/erebrus-gateway/api/v1/subscription"
 	"github.com/gin-gonic/gin"
 )
@@ -17,5 +18,6 @@ func ApplyRoutes(r *gin.RouterGroup) {
 		nodes.ApplyRoutes(v1)
 		subscription.ApplyRoutes(v1)
 		nodeOperatorForm.ApplyRoutes(v1)
+		registerDeFi.ApplyRoutes(v1)
 	}
 }
