58d4ea5ab0543e3bc1ac7326fd3d6c09ee090483
Shubham Prajapati <shubhamprajapati032@gmail.com>
2024-12-18 20:27:14 +0530

    `Added new endpoint to fetch nodes by status and wallet address`

--- api/v1/nodes/nodes.go
+++ api/v1/nodes/nodes.go
@@ -2,6 +2,7 @@ package nodes
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/NetSepio/erebrus-gateway/config/dbconfig"
@@ -16,6 +17,7 @@ func ApplyRoutes(r *gin.RouterGroup) {
 	{
 		g.GET("/all", FetchAllNodes)
 		g.GET("/:status", FetchAllNodesByStatus)
+		g.GET("/status_wallet_address/:status/:wallet_address", FetchAllNodesByStatusAndWalletAddress)
 	}
 }
 
@@ -91,6 +93,88 @@ func FetchAllNodesByStatus(c *gin.Context) {
 		logwrapper.Errorf("failed to get nodes from DB: %s", err)
 		httpo.NewErrorResponse(http.StatusInternalServerError, err.Error()).SendD(c)
 		return
+	}
+
+	// Unmarshal SystemInfo into OSInfo struct
+
+	var responses []models.NodeResponse
+	var response models.NodeResponse
+
+	for _, i := range *nodes {
+		var osInfo models.OSInfo
+		if len(i.SystemInfo) > 0 {
+			err := json.Unmarshal([]byte(i.SystemInfo), &osInfo)
+			if err != nil {
+				logwrapper.Errorf("failed to get nodes from DB OSInfo: %s", err)
+				// httpo.NewErrorResponse(http.StatusInternalServerError, err.Error()).SendD(c)
+			}
+		}
+		// Unmarshal IpInfo into IPInfo struct
+		var ipGeoAddress models.IpGeoAddress
+		if len(i.IpGeoData) > 0 {
+			err := json.Unmarshal([]byte(i.IpGeoData), &ipGeoAddress)
+			if err != nil {
+				logwrapper.Errorf("failed to get nodes from DB IpGeoAddress: %s", err)
+				// httpo.NewErrorResponse(http.StatusInternalServerError, err.Error()).SendD(c)
+			}
+		}
+
+		response.Id = i.PeerId
+		response.Name = i.Name
+		response.HttpPort = i.HttpPort
+		response.Domain = i.Host
+		response.NodeName = i.Name
+		response.Address = i.PeerAddress
+		response.Region = i.Region
+		response.Status = i.Status
+		response.DownloadSpeed = i.DownloadSpeed
+		response.UploadSpeed = i.UploadSpeed
+		response.StartTimeStamp = i.RegistrationTime
+		response.LastPingedTimeStamp = i.LastPing
+		response.Chain = i.Chain
+		response.WalletAddressSui = i.WalletAddress
+		response.WalletAddressSolana = i.WalletAddress
+		response.IpInfoIP = ipGeoAddress.IpInfoIP
+		response.IpInfoCity = ipGeoAddress.IpInfoCity
+		response.IpInfoCountry = ipGeoAddress.IpInfoCountry
+		response.IpInfoLocation = ipGeoAddress.IpInfoLocation
+		response.IpInfoOrg = ipGeoAddress.IpInfoOrg
+		response.IpInfoPostal = ipGeoAddress.IpInfoPostal
+		response.IpInfoTimezone = ipGeoAddress.IpInfoTimezone
+
+		responses = append(responses, response)
+	}
+
+	httpo.NewSuccessResponseP(200, "Nodes fetched succesfully", responses).SendD(c)
+}
+
+func FetchAllNodesByStatusAndWalletAddress(c *gin.Context) {
+	status := c.Param("status")                // active , inactive
+	walletAddress := c.Param("wallet_address") // active , inactive
+
+	fmt.Printf("status : %v , wallet address : %v ", status, walletAddress)
+
+	if len(walletAddress) == 0 || walletAddress == "wallet_address" {
+		logwrapper.Errorf("please pass the wallet address : ", walletAddress)
+		httpo.NewErrorResponse(http.StatusBadRequest, "Please provide the wallet_address").SendD(c)
+		return
+	}
+
+	db := dbconfig.GetDb()
+	var nodes *[]models.Node
+	// var node *models.Node
+	if len(status) == 0 || status == ":status" {
+		if err := db.Where("wallet_address = ?", walletAddress).Find(&nodes).Error; err != nil {
+			logwrapper.Errorf("failed to get nodes from DB: %s", err)
+			httpo.NewErrorResponse(http.StatusInternalServerError, err.Error()).SendD(c)
+			return
+		}
+	} else {
+		if err := db.Where("wallet_address = ? AND status = ? ", walletAddress, status).Find(&nodes).Error; err != nil {
+			logwrapper.Errorf("failed to get nodes from DB: %s", err)
+			httpo.NewErrorResponse(http.StatusInternalServerError, err.Error()).SendD(c)
+			return
+		}
 	}
 
 	// Unmarshal SystemInfo into OSInfo struct
