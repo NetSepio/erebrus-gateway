feb7629c3101366ff6c11293a3d5c85caca16918
Shubham Prajapati <shubhamprajapati032@gmail.com>
2025-03-06 16:39:03 +0530

    `Remove gorm import and gorm.DeletedAt field from PerkNFT and PerksToken structs`

--- models/perk.nft.go
+++ models/perk.nft.go
@@ -4,17 +4,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type PerkNFT struct {
-	ID              uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
-	Blockchain      string         `gorm:"type:varchar(50);not null" json:"blockchain"`
-	Name            string         `gorm:"type:varchar(100);not null" json:"name"`
-	Symbol          string         `gorm:"type:varchar(50);not null" json:"symbol"`
-	ImageURI        string         `gorm:"type:text;not null" json:"imageUri"`
-	ContractAddress string         `gorm:"type:varchar(100);not null" json:"contractAddress"`
-	CreatedAt       time.Time      `json:"createdAt"`
-	UpdatedAt       time.Time      `json:"updatedAt"`
-	DeletedAt       gorm.DeletedAt `gorm:"index" json:"-"`
+	ID              uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	Blockchain      string    `gorm:"type:varchar(50);not null" json:"blockchain"`
+	Name            string    `gorm:"type:varchar(100);not null" json:"name"`
+	Symbol          string    `gorm:"type:varchar(50);not null" json:"symbol"`
+	ImageURI        string    `gorm:"type:text;not null" json:"imageUri"`
+	ContractAddress string    `gorm:"type:varchar(100);not null" json:"contractAddress"`
+	CreatedAt       time.Time `json:"createdAt"`
+	UpdatedAt       time.Time `json:"updatedAt"`
 }
